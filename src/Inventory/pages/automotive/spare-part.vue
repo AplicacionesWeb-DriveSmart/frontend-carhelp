941bc4ab4afe681e24b4b5aa2f6b1808675a92c5
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-05-01 02:16:09 -0500

    docs: docs updated

--- src/Inventory/pages/automotive/spare-part.vue
+++ src/Inventory/pages/automotive/spare-part.vue
@@ -10,11 +10,20 @@ export default {
   <pv-card>
     <template #content>
       <div class="spare-part-card">
-        <img :src="sparepart.image" alt="spare-part image" >
+        <img :src="sparepart.image" alt="spare-part image" width="300px">
         <h1>Name: {{ sparepart.name }}</h1>
-        <p>Author: {{ sparepart.description }}</p>
-        <p>Width: {{ sparepart.price }}</p>
-        <p>Height: {{ sparepart.quantity }}</p>
+        <p>Description: {{ sparepart.description }}</p>
+        <p>Price: {{ sparepart.price }}</p>
+        <p>Quantity: {{ sparepart.quantity }}</p>
+        <pv-button label="MORE" style="
+            padding: 10px 10px; /* Adjust padding to increase button size */
+            font-size: 30px; /* Increase font size */
+            border: none;
+            background-color: black; /* Button background color */
+            color: white !important; /* Button text color */
+            cursor: pointer;
+            border-radius: 8px; /* Rounded corners */
+            transition: background-color 0.3s ease;"></pv-button>
       </div>
     </template>
   </pv-card>
@@ -24,6 +33,7 @@ export default {
 .spare-part-card {
   background-color: gray;
   padding: 20px;
+  max-width: 500px;
 }
 .spare-part-card img {
   max-width: 100%;
